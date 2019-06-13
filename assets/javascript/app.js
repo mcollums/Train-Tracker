21d3329d4db01fe11b0524ec1f65231979e830f1
mcollums <collumsmichelle@gmail.com>
2019-06-13 10:04:55 -0500

    Created add-btn and child-added functino
    When the add btn is clicked, that information is added to the firebase. Added a database reference that will update when there's a child added. Added form controls to the html.

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -11,12 +11,81 @@ const firebaseConfig = {
 firebase.initializeApp(firebaseConfig);
 var database = firebase.database();
 
-var name = "";
+var num = "";
 var line = "";
 var destination = "";
 var firstDept = "";
 var freq = "";
+var platform = ""
 var cost = 0;
+var currentTime = 0; //Will get this value from moment.js? Timestamp?
+
 //Will figure out Next Arrival and Time to Arrival by...
-    //Next Arrival: 
-    //Time to Arrival: 
+//Next Arrival: Start time and Frequency
+//Time to Arrival: Next Arrival - Current Time
+
+//Only allows # to be typed
+function isNumber(evt) {
+    var iKeyCode = (evt.which) ? evt.which : evt.keyCode
+    if (iKeyCode != 46 && iKeyCode > 31 && (iKeyCode < 48 || iKeyCode > 57)) {
+        alert("Only Numbers, Please");
+        return false;
+    } else {
+        return true; 
+    }
+}
+
+$("#add-btn").on("click", function (event) {
+    //Prevents the add button from reloading the page
+    event.preventDefault();
+
+    //Get values from the form
+    num = $("#num-input").val().trim();
+    line = $("#line-input").val().trim();
+    destination = $("#dest-input").val().trim();
+    firstDept = $("#start-time-input").val().trim();
+    freq = $("#freq-input").val().trim();
+    platform = $("#plat-input").val().trim();
+    cost = $("#cost-input").val().trim();
+
+    console.log(num + " " + line + " " + destination + " " + firstDept +
+        " " + freq + " " + platform + " " + cost);
+
+    //Push these values to the database
+    database.ref().push({
+        num: num,
+        line: line,
+        destination: destination,
+        firstDept: firstDept,
+        freq: freq,
+        platform: platform,
+        cost: cost,
+        dateAdded: firebase.database.ServerValue.TIMESTAMP
+    });
+});
+
+rowCount = 1;
+
+database.ref().on("child_added", function (snapshot) {
+    currentRow = "#row" + rowCount;
+    console.log(snapshot.child());
+    $("tbody").append("<tr><th id=\"row" + rowCount + "/>" +
+        snapshot.val().num + "</th>" + "<td>" +
+        snapshot.val().line + "</td>" + "<td>" +
+        snapshot.val().destination + "</td>" +
+        "<td> Next Arrival Would Go Here </td>" +
+        "<td> Time to Arrival Would Go Here </td>" +
+        "<td>" + snapshot.val().freq + " min</td>" +
+        "<td>" + snapshot.val().platform + "</td>"
+    )
+    if(snapshot.val().cost.charAt(0) !== "$") {
+        $(currentRow).append( "<td> $ " + snapshot.val().platform + "</td>");
+    } else {
+        $(currentRow).append( "<td>" + snapshot.val().platform + "</td>");
+    }   
+    rowCount++;
+});
+
+
+
+
